26ea432ec009d6f76bc2392ac9788d525f28ff12
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-02-11 23:00:40 -0800

    implemented textbox and list that will hold the city suggestions

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
-  test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
-    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
   });
 
+  // Scenario 1: Show Upcoming Events
+  test('render list of events', () => {
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+  // Scenario 2: Show a List of Suggestions During Search
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
