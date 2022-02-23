e4378b0fa1598a25e090fd9fb5aba51e8abbc23f
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-02-23 07:07:48 -0800

    updated App.test.js, App.js, and CitySearch.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import { getEvents } from './api';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
   state = {
@@ -10,9 +10,25 @@ class App extends Component {
     locations: []
   }
 
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+      this.setState({ events, locations: extractLocations(events) 
+    });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => event.location === location);
+      const locationEvents = (location === 'all') ?
+      events :
+      events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents
       });
