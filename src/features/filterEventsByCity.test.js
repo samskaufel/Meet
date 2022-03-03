05609b21d73c1c3b5beed895bee966dc88738ef4
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-03-03 07:29:33 -0800

    Update filterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
-import { CitySearch } from '../CitySearch';
+import CitySearch from '../CitySearch';
 import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
